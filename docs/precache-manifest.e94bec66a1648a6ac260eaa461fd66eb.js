786acfd7c2919207b655608432e4f42ca47bda7c
krose <krose@tableau.com>
2019-07-08 11:32:56 -0700

    Updated to use react-scripts

---
+++ docs/precache-manifest.e94bec66a1648a6ac260eaa461fd66eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68da949a401222b50b5e7e738469bc42",
+    "url": "/extension-filter-bookmarks/index.html"
+  },
+  {
+    "revision": "0770e4aa82ca1c1b9e24",
+    "url": "/extension-filter-bookmarks/static/css/main.2a445f43.chunk.css"
+  },
+  {
+    "revision": "d39d8ead863a20487d95",
+    "url": "/extension-filter-bookmarks/static/js/2.b387ec16.chunk.js"
+  },
+  {
+    "revision": "0770e4aa82ca1c1b9e24",
+    "url": "/extension-filter-bookmarks/static/js/main.947aaac0.chunk.js"
+  },
+  {
+    "revision": "5ab52acecae00858986a",
+    "url": "/extension-filter-bookmarks/static/js/runtime~main.2daab69c.js"
+  }
+]);
