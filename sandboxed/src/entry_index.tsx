ddaec40ef69c1bcd2fcad0479d779adada803ff2
krose <krose@tableau.com>
2019-11-18 12:31:28 -0800

    Fixed config reload issue and updated lib (all for sandboxed only)
    Fixed double index.tsx load which was causing config to pop open repeatedly.
    Updated to version 1.3 of extensions lib.

---
+++ sandboxed/src/entry_index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import FilterBookmarks from './FilterBookmarks';
+import './style.css';
+
+ReactDOM.render(<FilterBookmarks />, document.getElementById('container'));
--- sandboxed/src/index.tsx
+++ sandboxed/src/index.tsx
@@ -1,6 +1,1 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import FilterBookmarks from './FilterBookmarks';
-import './style.css';
-
-ReactDOM.render(<FilterBookmarks />, document.getElementById('container'));
+export { }
